ddfe6d3e9688d9331d4bff4f2a6a83e883648c6c
Sherwin Rhey Condez <shervs336@gmail.com>
2017-10-10 11:35:38 +0800

    Fix jquery src.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('bower_components/jquery/dist/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
     <script src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 </body>
 </html>
